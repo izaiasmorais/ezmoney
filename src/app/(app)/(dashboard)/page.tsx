91822b645b0257449a78cdc4b09b44602361b6b8
Izaías Lima <izaiaslima356@gmail.com>
2023-10-27 20:17:59 -0300

    Update page.tsx

--- src/app/(app)/(dashboard)/page.tsx
+++ src/app/(app)/(dashboard)/page.tsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
 						</strong>
 					</div>
 
-					<ProfitChart />
+{/* 					<ProfitChart /> */}
 				</Card>
 
 				<Card className="p-4 md:p-6 space-y-4 md:space-y-6">
